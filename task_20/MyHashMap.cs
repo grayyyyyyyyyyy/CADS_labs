45514375278ffcc94a7a71e1505e1dbcedbf4840
grayyyyyyyyyyy <serjvlasov0712@gmail.com>
2024-12-06 01:36:03 +0300

    Add files via upload

---
+++ task_20/MyHashMap.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMap
+{
+    public class MyHashMap<K, V>
+    {
+        private class Entry
+        {
+            public K key;
+            public V value;
+            public Entry next;
+            public Entry(K key, V value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+        Entry[] table;
+        int size;
+        double loadFactor;
+
+        //1
+        public MyHashMap()
+        {
+            table = new Entry[16];
+            size = 16;
+            loadFactor = 0.75;
+        }
+        //2
+        public MyHashMap(int initialCapacity)
+        {
+            table = new Entry[initialCapacity];
+            size = initialCapacity;
+            loadFactor = 0.75;
+        }
+        //3
+        MyHashMap(int initialCapacity, double loadFactor)
+        {
+            table = new Entry[initialCapacity];
+            size = initialCapacity;
+            this.loadFactor = loadFactor;
+        }
+
+        private int GetHashCode(K key) => Math.Abs(key.GetHashCode()) % table.Length;
+        private int GetHashCode(V value) => Math.Abs(value.GetHashCode()) % table.Length;
+        private void HelpPut(K key, V value)
+        {
+            int index = GetHashCode(key);
+            Entry step = table[index];
+            if (step != null)
+            {
+                int f1 = 1;
+                while (step.next != null)
+                {
+                    if (step.key.Equals(key))
+                    {
+                        step.value = value;
+                        f1 = 0;
+                    }
+                    step = step.next;
+                }
+                if (step.key.Equals(key))
+                {
+                    step.value = value;
+                    f1 = 0;
+                }
+                if (f1 == 1)
+                {
+                    Entry tmp = new Entry(key, value);
+                    step.next = tmp;
+                    step = tmp;
+                    size++;
+                }
+            }
+            else
+            {
+                Entry tmp = new Entry(key, value);
+                table[index] = tmp;
+                size++;
+            }
+        }
+        private void PutInNew(Entry[] entries, K key, V value)
+        {
+            int index = Math.Abs(key.GetHashCode()) % entries.Length;
+            Entry tmp = new Entry(key, value);
+            if (entries[index] != null)
+            {
+                Entry step = entries[index];
+                while (step.next != null) step = step.next;
+                step.next = tmp;
+            }
+            else entries[index] = tmp;
+            size++;
+        }
+        private void Resize()
+        {
+            Entry[] newEntries = new Entry[table.Length * 3];
+            int prevSize = size;
+            size = 0;
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] != null)
+                {
+                    Entry value = table[i];
+                    while (value != null)
+                    {
+                        int index = Math.Abs(value.key.GetHashCode()) % newEntries.Length;
+                        Entry nextValue = value.next;
+                        PutInNew(newEntries, value.key, value.value);
+                        value = nextValue;
+                    }
+                }
+            }
+            table = newEntries;
+        }
+        //4
+        public void Clear() => size = 0;
+
+        //5
+        public bool ContainsKey(K key)
+        {
+            int index = GetHashCode(key);
+            Entry step = table[index];
+            while (step != null)
+            {
+                if (step.key.Equals(key)) return true;
+                step = step.next;
+            }
+            return false;
+        }
+        //6
+        public bool ContainsValue(V value)
+        {
+            int index = GetHashCode(value);
+            Entry step = table[index];
+            while (step != null)
+            {
+                if (step.value.Equals(value)) return true;
+                step = step.next;
+            }
+            return false;
+        }
+        //7
+        public IEnumerable<KeyValuePair<K, V>> EntrySet()
+        {
+            foreach (var entry in table)
+            {
+                for (var step = entry; step != null; step = step.next) yield return new KeyValuePair<K, V>(step.key, step.value);
+            }
+        }
+        //8
+        public V Get(K key)
+        {
+            int index = GetHashCode(key);
+            Entry step = table[index];
+            while (step != null)
+            {
+                if (step.key.Equals(key)) return step.value;
+                step = step.next;
+            }
+            throw new KeyNotFoundException();
+        }
+        //9
+        public bool IsEmpty() => size == 0;
+        //10
+        public K[] KeySet()
+        {
+            K[] array = new K[size];
+            int index = 0;
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] != null)
+                {
+                    Entry step = table[i];
+                    while (step != null)
+                    {
+                        array[index++] = step.key;
+                        step = step.next;
+                    }
+                }
+            }
+            return array;
+        }
+        //11
+        public void Put(K key, V value)
+        {
+            double count = (double)(size + 1) / (double)table.Length;
+            if (count >= loadFactor) Resize();
+            HelpPut(key, value);
+        }
+        //12
+        public void Remove(K key)
+        {
+            int index = GetHashCode(key);
+            Entry step = table[index];
+            if (step != null) return;
+            if (step.key.Equals(key))
+            {
+                table[index] = table[index].next;
+                size--;
+                return;
+            }
+            Entry current = table[index];
+            Entry previous = null;
+            while (current != null)
+            {
+                if (current.key.Equals(key))
+                {
+                    previous.next = current.next;
+                    size--;
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+        }
+        //13
+        public int Size() => size;
+
+    }
+}
---
+++ task_20/Program.cs
@@ -0,0 +1,33 @@
+﻿using HashMap;
+using System.Text.RegularExpressions;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyHashMap<string, string> defs = new MyHashMap<string, string>();
+        string path = "input.txt";
+        StreamReader sr = new StreamReader(path);
+        string? line = sr.ReadLine();
+
+        string pattern = @"(double|int|float) \D+\S* = ?(\S)+?(?=;)";
+
+        while (line != null)
+        {
+            MatchCollection matches = Regex.Matches(line, pattern);
+            foreach(Match match in matches)
+            {
+                string[] part = match.Value.Split(' ');
+                string type = part[0].Trim();
+                string value = part[3].Trim();
+                string name = part[1].Trim();
+                string typeValue = type + " " + value;
+                if (defs.ContainsKey(name)) Console.WriteLine($"Redefenition attempt: {type} {name} = {value} ");
+                else defs.Put(name, typeValue);
+            }
+            line = sr.ReadLine();
+        }
+        sr.Close();
+        IEnumerable<KeyValuePair<string, string>> ans = defs.EntrySet();
+        foreach(var pair in ans) Console.WriteLine($"{pair.Value.Split(' ')[0]} = {pair.Key} {pair.Value.Split(' ')[1]}");
+    }
+}
