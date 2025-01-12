5cd3a56737fcfde43bbf268c223c480cb7bd6a1b
grayyyyyyyyyyy <serjvlasov0712@gmail.com>
2025-01-12 19:57:28 +0300

    created

---
+++ task_28/Program.cs
@@ -0,0 +1,15 @@
+﻿using Library;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        MyArrayList<int> arrayList = new MyArrayList<int>(array);
+        var iter1 = arrayList.IteratorList();
+        while (iter1.HasNext())
+        {
+            iter1.Next();
+            Console.Write(iter1.Cursor + " ");
+        }
+    }
+}
